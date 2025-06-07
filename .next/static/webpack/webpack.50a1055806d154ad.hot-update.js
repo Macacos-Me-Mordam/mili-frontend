367770c713f94301d853648d7644c22c507fae8a
psychovv <viniciusmedina@gmail.com>
2025-06-07 10:44:23 -0300

    feat: add Radix UI components and implement sidebar functionality
    - Added new dependencies for Radix UI components including Dialog, Separator, Slot, and Tooltip.
    - Removed favicon.ico file.
    - Created reusable Button, Input, Separator, Sheet, and Skeleton components.
    - Implemented Sidebar component with context for managing open/closed state and mobile responsiveness.
    - Added hooks for detecting mobile view.
    - Introduced Tooltip component for enhanced user interaction.

--- .next/static/webpack/webpack.50a1055806d154ad.hot-update.js
+++ .next/static/webpack/webpack.50a1055806d154ad.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("be0b84309bddcd9f")
+/******/ 	__webpack_require__.h = () => ("8b62229dc694fc8b")
 /******/ })();
 /******/ 
 /******/ }
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/sebastiaokomada/Documents/mili-infra/mili-frontend/src/app/page.tsx
+// File: /home/medina/projects/mili-infra/mili-frontend/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
